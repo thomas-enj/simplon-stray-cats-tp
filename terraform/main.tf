71afe6ae0811ab0b6a49d9b1102f473b2d0d5f3a
Adri-Sig <eidowardo31@gmail.com>
2026-07-23 16:16:02 +0200

    Infra: Add variables to module

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,10 @@ module "network" {
   resource_group_name            = var.resource_group_name
   storage_account_container_name = var.storage_account_container_name
   owner                          = var.owner
+  tags                           = var.tags
+  team                           = var.team
+  location                       = var.location
+
 
 }
 
